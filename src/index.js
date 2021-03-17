257c758a73d969304aa0cde6f30b8b7bb9561de9
Nikita Sandhu <sandhunikita0096@gmail.com>
2021-03-17 13:10:28 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function LoginForm(props) {
+  const nameEl = React.useRef(null);
+  const passwordEl = React.useRef(null);
+  const rememberMeEl = React.useRef(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formData = {
+      username: nameEl.current.value,
+      password: passwordEl.current.value,
+      rememberMe: rememberMeEl.current.checked,
+    }
+    console.log(formData);
+  };
+
+  return (
+     <form onSubmit={handleSubmit}>
+       <p>User Name: <input type="text" placeholder="username" ref={nameEl} /></p>
+       <p>Password: <input type="password" placeholder="password" ref={passwordEl}/></p>
+       <label>
+         <input type="checkbox" ref={rememberMeEl} />
+         Remember me
+       </label>
+       <p><button type="submit" className="myButton">Login</button></p>
+     </form>
+   );
+}
+
+ReactDOM.render(<div><LoginForm /></div>, document.getElementById("root"));
